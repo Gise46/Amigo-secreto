b18b8097886daeba6da9847289db5d6cd3078438
Gisela Fernández <116768877+Gise46@users.noreply.github.com>
2025-09-22 19:27:40 -0300

    Update app.js

--- app.js
+++ app.js
@@ -3,8 +3,17 @@ let amigos = [];
 function agregarAmigo(nombre) {
   let imputAmigo = document.getElementById("amigo");
   let amigo = inputAmigo.value;
-  console.log(amigo);
+
+  if (amigo === "") {
+   alert(Por Favor, Ingresa el nombre de un amigo.");
+    return;
+   } 
+  
+  amigos.push(amigo);
+  
+  console.log(amigos);
 }
+
 
 
 
